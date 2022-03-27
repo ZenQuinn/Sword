8149a4649d62a63f049195084cde50c2f2d0b756
FiercePC <FiercePC@IZZIECOMPUTER>
2022-03-27 20:49:15 +0100

    Add project files.

---
+++ lightsaber.cpp
@@ -0,0 +1,29 @@
+// lightsaber.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "            \n";
+    cout << "    /j      \n";
+    cout << "   /  j     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "   |  |     \n";
+    cout << "  ------    \n";
+    cout << " {      }   \n";
+    cout << "  ______    \n";
+    cout << "   ||||     \n";
+    cout << "   ||||     \n";
+    cout << "   ||||     \n";
+    cout << "   ||||     \n";
+    cout << "   l  /     \n";
+    cout << "    --      \n";
+    cout << "            \n";
+}
